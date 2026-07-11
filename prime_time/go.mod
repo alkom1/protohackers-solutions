07c45de7c6f7323e9e7dfa704840aa49487b2702
alkom1 <17685430+alkom1@users.noreply.github.com>
2026-07-11 22:36:24 +0200

    prime time - first test

---
+++ prime_time/go.mod
@@ -0,0 +1,3 @@
+module github.com/alkom1/protohackers/prime_time
+
+go 1.26.5
---
+++ prime_time/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+)
+
+type response struct {
+	Method string `json:"method"`
+	Prime  bool   `json:"prime"`
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":9901")
+	if err != nil {
+		log.Fatal("Error listening:", err)
+	}
+
+	defer listener.Close()
+
+	fmt.Println("Running...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+	text, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Println("Error reading line:", err)
+		return
+	}
+
+	var dat map[string]interface{}
+	if err = json.Unmarshal([]byte(text), &dat); err != nil {
+		log.Println("JSON decoding error:", err)
+		return
+	}
+
+	//method := dat["method"].(string)
+	number := dat["number"].(int)
+
+	res := &response{
+		Method: "isPrime",
+		Prime:  isPrime(number),
+	}
+	b, err := json.Marshal(res)
+	if err != nil {
+		log.Println("Error JSONing:", err)
+		return
+	}
+	conn.Write(b)
+}
+
+func isPrime(n int) bool {
+	if n <= 1 {
+		return false
+	}
+	for i := 2; i*i <= n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+} // https://www.geeksforgeeks.org/go-language/how-to-find-prime-number-in-golang/
+
+// Based on https://gobyexample.com/json
