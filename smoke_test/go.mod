26e4fc42d1d616bc09a715de011353744b9160ff
alkom1 <17685430+alkom1@users.noreply.github.com>
2026-07-11 21:55:03 +0200

    smoke test - 1st version

---
+++ smoke_test/go.mod
@@ -0,0 +1,3 @@
+module github.com/alkom1/protohackers/smoke_test
+
+go 1.26.5
---
+++ smoke_test/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:9090")
+	if err != nil {
+		log.Fatal("Error listening:", err)
+	}
+
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	for {
+		b, err := reader.ReadByte()
+		if err != nil {
+			break
+		}
+		_, err = conn.Write([]byte{b})
+		if err != nil {
+			break
+		}
+	}
+
+	/*
+		bytes, err := reader.ReadBytes()
+		if err != nil {
+			log.Printf("Read error: %v", err)
+			return
+		}
+
+		_, err := conn.Write(bytes)
+		if err != nil {
+			log.Printf("Server write error: %v", err)
+		}
+	*/
+}
+
+// Based on https://gobyexample.com/tcp-server
