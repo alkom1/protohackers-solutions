b3cfe6ecdb40771b63b9a263655527b8cd7bd71e
alkom1 <17685430+alkom1@users.noreply.github.com>
2026-07-23 01:06:32 +0200

    prime time - error handling

--- prime_time/main.go
+++ prime_time/main.go
@@ -3,14 +3,19 @@ package main
 import (
 	"bufio"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net"
+	"strings"
 )
 
 type response struct {
 	Method string `json:"method"`
 	Prime  bool   `json:"prime"`
+}
+
+type request struct {
+	Method  string          `json:"method"`
+	NumberS json.RawMessage `json:"number"`
 }
 
 func main() {
@@ -21,7 +26,7 @@ func main() {
 
 	defer listener.Close()
 
-	fmt.Println("Running...")
+	log.Println("Running...")
 
 	for {
 		conn, err := listener.Accept()
@@ -38,35 +43,60 @@ func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
 	reader := bufio.NewReader(conn)
-	text, err := reader.ReadString('\n')
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		text := scanner.Text()
+		log.Println(text)
 
-	fmt.Println(text)
+		var req request
+		if err := json.Unmarshal([]byte(text), &req); err != nil {
+			log.Println("JSON decoding error:", err)
+			conn.Write([]byte("{}\n"))
+			return
+		}
 
-	if err != nil {
-		log.Println("Error reading line:", err)
-		return
+		if req.Method != "isPrime" {
+			log.Println("not isPrime")
+			conn.Write([]byte("{}\n"))
+			return
+		}
+
+		if strings.ContainsAny(string(req.NumberS), "\"'") {
+			log.Println("not number")
+			conn.Write([]byte("{}\n"))
+			return
+		}
+
+		if strings.ContainsAny(string(req.NumberS), ".") {
+			log.Println("not integer")
+			conn.Write([]byte("{\"method\":\"isPrime\",\"prime\":false}\n"))
+			return
+		}
+
+		var number int
+		if err := json.Unmarshal(req.NumberS, &number); err != nil {
+			log.Println("error int parsing:", err)
+			conn.Write([]byte("{}\n"))
+			return
+		}
+
+		res := &response{
+			Method: "isPrime",
+			Prime:  isPrime(number),
+		}
+		b, err := json.Marshal(res)
+		if err != nil {
+			log.Println("Error JSONing:", err)
+			return
+		}
+		log.Println("sending:", string(b))
+		conn.Write(b)
+		conn.Write([]byte{'\n'})
 	}
 
-	var dat map[string]interface{}
-	if err = json.Unmarshal([]byte(text), &dat); err != nil {
-		log.Println("JSON decoding error:", err)
-		return
+	if err := scanner.Err(); err != nil {
+		log.Fatal("scanner err:", err)
 	}
-
-	//method := dat["method"].(string)
-	numberFloat := dat["number"].(float64)
-	number := (int)(numberFloat)
-
-	res := &response{
-		Method: "isPrime",
-		Prime:  isPrime(number),
-	}
-	b, err := json.Marshal(res)
-	if err != nil {
-		log.Println("Error JSONing:", err)
-		return
-	}
-	conn.Write(b)
 }
 
 func isPrime(n int) bool {
