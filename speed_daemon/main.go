e524129eb4f1e29e25d15918d45475a04fa961cc
alkom1 <17685430+alkom1@users.noreply.github.com>
2026-07-24 16:54:00 +0200

    speed daemon - this went zero to hundred real fast

---
+++ speed_daemon/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":9906")
+	if err != nil {
+		log.Fatal("Error listening:", err)
+	}
+
+	defer listener.Close()
+
+	log.Println("Running...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+}
+
+// PUZZLE: https://protohackers.com/problem/6
